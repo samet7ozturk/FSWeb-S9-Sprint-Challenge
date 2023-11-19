5c58b19119aaa3db6022b8c7df0d06377d13eb30
samet7ozturk <sametozturk973@gmail.com>
2023-11-19 23:55:49 +0300

    Complete

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,28 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
+import axios from "axios";
 
 // önerilen başlangıç stateleri
 const initialMessage = "";
 const initialEmail = "";
 const initialSteps = 0;
-const initialIndex = [2, 2]; //  "B" nin bulunduğu indexi
+const initiaCoordinate = [2, 2]; //  "B" nin bulunduğu indexi
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email zorunludur!")
+    .email("Geçerli email yazınız!"),
+});
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
   const [message, setMessage] = useState(initialMessage);
   const [email, setEmail] = useState(initialEmail);
-  const [steps, setSteps] = useState(initialSteps);
-  const [currentIndex, setCurrentIndex] = useState(initialIndex);
+  const [steps, setSteps] = useState(0);
+  const [coordinate, setCoordinate] = useState(initiaCoordinate);
+  const [direction, setDirection] = useState([]);
+  const [index, setIndex] = useState(4);
+  const [isError, setIsError] = useState(true);
+  const [errorMessages, setErrorMessages] = useState("");
 
-  function getXY() {
-    // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
-    // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
-  }
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(email);
 
-  function getXYMesaj() {
-    // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
-    // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
+    axios
+      .post("http://localhost:9000/api/result", {
+        x: coordinate[1],
+        y: coordinate[0],
+        steps: steps,
+        email: email,
+      })
+      .then(function (response) {
+        setMessage(response.data.message);
+        setEmail("");
+      })
+      .catch(function (error) {
+        console.log(error.response.data.message);
+        setMessage(error.response.data.message);
+      });
   }
 
   function reset() {
@@ -30,53 +51,180 @@ export default function AppFunctional(props) {
     setMessage(initialMessage);
     setEmail(initialEmail);
     setSteps(initialSteps);
-    setCurrentIndex(initialIndex); //  "B" nin bulunduğu indexi
+    setCoordinate(initiaCoordinate);
+    setDirection([]);
   }
 
-  function sonrakiIndex(yon) {
-    // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
-    // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
-    // şu anki indeksi değiştirmemeli.
-  }
+  useEffect(() => {
+    if (direction[0] == "right") {
+      if (coordinate[1] <= 2) {
+        setCoordinate([coordinate[0], coordinate[1] + 1]);
+        setSteps(steps + 1);
+        setMessage("");
+      } else {
+        setMessage("Sağa gidemezsiniz");
+      }
+      if (coordinate[1] == 1) {
+        setMessage("");
+      }
+    } else if (direction == "up") {
+      if (coordinate[0] >= 2) {
+        setCoordinate([coordinate[0] - 1, coordinate[1]]);
+        setSteps(steps + 1);
+        setMessage("");
+      } else {
+        setMessage("Yukarıya gidemezsiniz");
+      }
 
-  function ilerle(evt) {
-    // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
-    // ve buna göre state i değiştirir.
-  }
+      if (coordinate[0] == 3) {
+        setMessage("");
+      }
+    } else if (direction == "left") {
+      if (coordinate[1] >= 2) {
+        setCoordinate([coordinate[0], coordinate[1] - 1]);
+        setSteps(steps + 1);
+        setMessage("");
+      } else {
+        setMessage("Sola gidemezsiniz");
+      }
 
-  function onChange(evt) {
-    // inputun değerini güncellemek için bunu kullanabilirsiniz
-  }
+      if (coordinate[1] == 3) {
+        setMessage("");
+      }
+    } else if (direction == "down") {
+      if (coordinate[0] <= 2) {
+        setCoordinate([coordinate[0] + 1, coordinate[1]]);
+        setSteps(steps + 1);
+        setMessage("");
+      } else {
+        setMessage("Aşağıya gidemezsiniz");
+      }
+      if (coordinate[0] == 1) {
+        setMessage("");
+      }
+    }
 
-  function onSubmit(evt) {
-    // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
-  }
+    console.log("direction:", direction);
+  }, [direction]);
+
+  useEffect(() => {
+    if (coordinate[0] == 1 && coordinate[1] == 1) {
+      setIndex(0);
+    } else if (coordinate[0] == 1 && coordinate[1] == 2) {
+      setIndex(1);
+    } else if (coordinate[0] == 1 && coordinate[1] == 3) {
+      setIndex(2);
+    } else if (coordinate[0] == 2 && coordinate[1] == 1) {
+      setIndex(3);
+    } else if (coordinate[0] == 2 && coordinate[1] == 2) {
+      setIndex(4);
+    } else if (coordinate[0] == 2 && coordinate[1] == 3) {
+      setIndex(5);
+    } else if (coordinate[0] == 3 && coordinate[1] == 1) {
+      setIndex(6);
+    } else if (coordinate[0] == 3 && coordinate[1] == 2) {
+      setIndex(7);
+    } else if (coordinate[0] == 3 && coordinate[1] == 3) {
+      setIndex(8);
+    }
+    console.log("coordinate: ", coordinate);
+  }, [coordinate]);
+
+  useEffect(() => {
+    validationSchema
+      .validate({ email })
+      .then((response) => {
+        setIsError(false);
+        console.log("hata yok ", response);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+        setIsError(true);
+        setErrorMessages(error.message);
+      });
+  }, [email]);
+
+  useEffect(() => {
+    validationSchema
+      .validate({ email })
+      .then((response) => {
+        setIsError(false);
+        console.log("hata yok ", response);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+        setIsError(true);
+        setErrorMessages(error.message);
+      });
+  }, [email]);
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">
+          Koordinatlar ({coordinate[1]}, {coordinate[0]})
+        </h3>
+        <h3 id="steps">{steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
-          <div key={idx} className={`square${idx === 4 ? " active" : ""}`}>
-            {idx === 4 ? "B" : null}
+          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
+            {idx === index ? "B" : null}
           </div>
         ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button
+          id="left"
+          onClick={() => {
+            setDirection(["left"]);
+          }}
+        >
+          SOL
+        </button>
+        <button
+          id="up"
+          onClick={() => {
+            setDirection(["up"]);
+          }}
+        >
+          YUKARI
+        </button>
+        <button
+          id="right"
+          onClick={() => {
+            setDirection(() => {
+              setDirection(["right"]);
+            });
+          }}
+        >
+          SAĞ
+        </button>
+        <button
+          id="down"
+          onClick={() => {
+            setDirection(["down"]);
+          }}
+        >
+          AŞAĞI
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={submitHandler}>
+        <input
+          id="email"
+          type="email"
+          placeholder="email girin"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
